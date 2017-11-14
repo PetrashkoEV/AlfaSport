b6fccf48fec776aceeb433cea438f08c65d93c78
Eugen Petrashko <PetrashkoEV@gmail.com>
2017-11-14 15:06:54 +0300

    Initialize data base project

---
+++ Server/Alfasport.DataBase/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]               BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Title]            NVARCHAR (250) NOT NULL,
+    [BriefDescription] NVARCHAR (250) NULL,
+    [Description]      NVARCHAR (MAX) NULL,
+    [CreatedDate]      DATETIME2 (7)  CONSTRAINT [DF_Product_CreatedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
